741baaae36c5f9eac1a5a950692fb04583a0c005
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-10-27 20:46:21 -0300

    criando conexão com fauna db e cadastrando usuários

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,9 @@
+import { query as q} from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '@/services/fauna'
 
 export const authOption = {
   providers: [
@@ -8,7 +12,41 @@ export const authOption = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     })
-  ]
+  ],
+  callbacks:{
+    async signIn(user, account, profile){
+      const {email} = user.user
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true
+      }catch{
+        return false
+      }
+      
+      
+    }
+  }
 }
 
 const handler = NextAuth(authOption)
